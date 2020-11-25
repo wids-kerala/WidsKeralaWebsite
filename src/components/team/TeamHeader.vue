d7c337233be48442dccb99f5303d7a0f0baa250d
wids-kerala <wids-kerala@gmail.com>
2020-11-26 03:31:25 +0530

    commit on 'master'

--- src/components/team/TeamHeader.vue
+++ src/components/team/TeamHeader.vue
@@ -3,10 +3,11 @@
         <v-row align="center" class="py-0 my-0">
             <v-col md="12" sm="12" cols="12" class="py-0 my-0">
                 <p class="google-font mb-0" style="font-weight: 350;font-size:200%"><b>
-                    <span style="color: #1a73e8;">Our</span> Team </b> 
+                    <span style="color: #52b04f;">Our</span> Team </b> 
                 </p>
-                <p class="google-font mt-0 hidden-sm-and-down">Google is known all around the world. Everyone is 'googling', checking on 'maps' and communicating in 'gmail'. For simple users, they are services that just works, but not for us. Developers see much more: APIs, scalability issues, complex technology stacks. And that is what GDG is about.</p>
-                <p class="google-font mt-0 ">Our goal is to organize space to connect the best industry experts with Indian audience to boost development of IT. And Our Core Team is:</p>
+                <br>
+                <p class="google-font mt-0 hidden-sm-and-down">The Women in Data Science (WiDS) Worldwide conference aims to inspire and educate data scientists worldwide, regardless of gender, and support women in the field. This annual technical conference provides an opportunity to hear about the latest data science research and applications in a broad set of domains.</p>
+                <p class="google-font mt-0 ">WiDS Ambassadors are  organizers around the world who share similar values and missions to plan events that coincide with the Women in Data Science (WiDS) Worldwide conference. Our goal is to organize space to connect the best female industry experts with Indian audience to boost development of Data Science. And our core team is:</p>
                 
             </v-col>
         </v-row>
