e6ec35abcbc039e0265444bdb96a61211b2b9220
Julian <juliansarajoseph@gmail.com>
2020-12-08 12:36:17 +0530

    Update Footer.vue

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -67,13 +67,6 @@
                 {{ item.linkname }}
             </v-btn>
             <v-spacer></v-spacer>
-           <span class="subtitle-2 google-font"> Based on Project 
-          <a
-            href="https://github.com/gdg-x/aura/"
-            target="_blank"
-            rel="noreferrer"
-            style="text-decoration:none"
-          > Aura</a></span>
           </v-toolbar>
         </v-col>
       </v-row>
